5cd224a6eb4a39c3c3cd5b3ca6cee53bab449307
polina1441 <14042003dp@mail.ru>
2021-01-04 11:04:13 +0700

    working bnv

---
+++ app/src/main/java/com/example/balamut/ui/action/ActionFragment.kt
@@ -0,0 +1,31 @@
+package com.example.balamut.ui.action
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.balamut.R
+
+class ActionFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_action, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/balamut/ui/progress/ProgressFragment.kt
@@ -0,0 +1,31 @@
+package com.example.balamut.ui.progress
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.balamut.R
+
+class ProgressFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_progress, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
